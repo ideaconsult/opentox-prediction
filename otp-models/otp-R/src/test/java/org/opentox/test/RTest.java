eb7649c42497544e3e35dc0563f280531061c85e
ngn <jeliazkova.nina@gmail.com>
2012-02-20 20:24:37 +0200

    placeholder for R wrapper

---
+++ otp-models/otp-R/src/test/java/org/opentox/test/RTest.java
@@ -0,0 +1,27 @@
+package org.opentox.test;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import org.junit.Test;
+
+public class RTest {
+
+	@Test
+	public void test() throws Exception {
+		  // create a script engine manager
+	    ScriptEngineManager factory = new ScriptEngineManager();
+	  
+	    // create an R engine
+	    ScriptEngine engine = factory.getEngineByName("Renjin");
+	    
+	    // evaluate JavaScript code from String
+	    engine.eval("print('Hello, World')");
+		/*
+	
+	    engine.put("benchmarkArgs", new StringVector(args));
+	    
+	    engine.eval(new InputStreamReader(new FileInputStream("src/main/R/runner.R")));
+	    */
+	}
+}
