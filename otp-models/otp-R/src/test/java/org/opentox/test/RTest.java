ce171181d9c4d3ec000c51c0cce00325a44df4d4
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-02-20 20:42:38 +0200

    renjin dependencies commented out

--- otp-models/otp-R/src/test/java/org/opentox/test/RTest.java
+++ otp-models/otp-R/src/test/java/org/opentox/test/RTest.java
@@ -1,7 +1,7 @@
 package org.opentox.test;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+//import javax.script.ScriptEngine;
+//import javax.script.ScriptEngineManager;
 
 import org.junit.Test;
 
@@ -10,18 +10,18 @@ public class RTest {
 	@Test
 	public void test() throws Exception {
 		  // create a script engine manager
-	    ScriptEngineManager factory = new ScriptEngineManager();
+//	    ScriptEngineManager factory = new ScriptEngineManager();
 	  
 	    // create an R engine
-	    ScriptEngine engine = factory.getEngineByName("Renjin");
+//	    ScriptEngine engine = factory.getEngineByName("Renjin");
 	    
 	    // evaluate JavaScript code from String
-	    engine.eval("print('Hello, World')");
+//	    engine.eval("print('Hello, World')");
 		/*
 	
-	    engine.put("benchmarkArgs", new StringVector(args));
+//	    engine.put("benchmarkArgs", new StringVector(args));
 	    
-	    engine.eval(new InputStreamReader(new FileInputStream("src/main/R/runner.R")));
+//	    engine.eval(new InputStreamReader(new FileInputStream("src/main/R/runner.R")));
 	    */
 	}
 }
